1b6a14028f1d181e169eda065937cec8eab7c2fb
Dan Christopher <danchristopher12751@gmail.com>
2026-02-21 22:35:30 +0530

    made some minor changes in the frontend

--- practice-web/src/App.jsx
+++ practice-web/src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="min-h-screen bg-white">
+    <div className="app-wrapper">
       <Navbar />
     </div>
   );
 }
 
 export default App;
-
