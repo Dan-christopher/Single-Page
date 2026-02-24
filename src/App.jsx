c713bc6a8988894ae04f9351bb8218d6b211488b
Dan Christopher <danchristopher12751@gmail.com>
2026-02-24 23:35:13 +0530

    modified changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,16 @@ import './App.css'
 
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import TrustedBy from './components/TrustedBy'
+import Services from './components/Services'
+import WhyChooseUs from './components/WhyChooseUs'
+import HowItWorks from './components/HowItWorks'
+import CaseStudies from './components/CaseStudies'
+import Testimonials from './components/Testimonials'
+import Pricing from './components/Pricing'
+import FAQ from './components/FAQ'
+import FinalCTA from './components/FinalCTA'
+import Footer from './components/Footer'
 
 function App() {
   const [theme, setTheme] = useState(() => {
@@ -23,6 +33,16 @@ function App() {
     <div className="app-wrapper" data-theme={theme}>
       <Navbar theme={theme} toggleTheme={toggleTheme} />
       <Hero />
+      <TrustedBy />
+      <Services />
+      <WhyChooseUs />
+      <HowItWorks />
+      <CaseStudies />
+      <Testimonials />
+      <Pricing />
+      <FAQ />
+      <FinalCTA />
+      <Footer />
     </div>
   );
 }
